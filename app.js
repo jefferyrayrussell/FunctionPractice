4047778e1cec695e3cb6fdb4de46b93366f2e876
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-03-18 12:06:17 -0700

    Java Script Problem 3 in Process

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 
 function sum (x,y) {
   var total = x + y;
-  console.log('The sum of ' + x + ' and ' + y + ' is ' + total);
+  /*console.log('The sum of ' + x + ' and ' + y + ' is ' + total);*/
 };
 
 /* Problem 2  multiply() */
 
 function multiply (x,y) {
   var total = x * y;
-  console.log('The product of ' + x + ' and ' + y + ' is ' + total);
+  /*console.log('The product of ' + x + ' and ' + y + ' is ' + total);*/
 };
+
+/* Problem 3  sumAndMultiply() */
+
+var a = total;
+var b = (total / x) + x;
+
+function sumAndMultiply (x,y,z) {
+  var m = x + y + z;
+  var n = x * y * z;
+  var o = [a,b];
+  console.log(x + ' and ' + y + ' and ' + z + ' have a product of ' a)
+
+}
