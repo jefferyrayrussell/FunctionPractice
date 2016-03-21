2765ecb2fa51357aa0ab1d9bf7ee22792242c147
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-03-20 20:28:28 -0700

    Javascript worked after returns were placed in the code for Problems 1 & 2.

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-/* Problem 1  sum() */
+/* Problem 1  sum() takes in two numbers as arguments and returns their sum*/
 
 function sum (x,y) {
   var total = x + y;
   /*console.log('The sum of ' + x + ' and ' + y + ' is ' + total);*/
 };
 
-/* Problem 2  multiply() */
+/* Problem 2  multiply() takes in two numbers as arguments and returns their product*/
 
 function multiply (x,y) {
   var total = x * y;
-  /*console.log('The product of ' + x + ' and ' + y + ' is ' + total);*/
+    /*console.log('The product of ' + x + ' and ' + y + ' is ' + total);*/
 };
 
-/* Problem 3  sumAndMultiply() */
-
-var a = total;
-var b = (total / x) + x;
+/* Problem 3  sumAndMultiply() takes in three numbers as an argument and by using the above sum() and multiply() returns two values in an array; namely the sum and the prod*/
 
 function sumAndMultiply (x,y,z) {
-  var m = x + y + z;
-  var n = x * y * z;
-  var o = [a,b];
-  console.log(x + ' and ' + y + ' and ' + z + ' have a product of ' a)
-
-}
+  var a = sum((x + y),z);
+  var b = multiply((x * y),z);
+  console.log(x,y,z);
+  console.log(x + ' and ' + y + ' and ' + z + ' sum to ' + a);
+  console.log('The numbers ' + x + ' and ' + y + ' and ' + z + ' have a product of ' + b);
+  return [a,b];
+};
